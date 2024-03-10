d32eae8b07e3faa13a589b0327b746432eefdcd0
Jason Lyle <jason.lyle@oracle.com>
2024-03-10 11:57:59 -0400

    Add test for defaults file

---
+++ source/sqlcl_direct_unit_tests/sqlcl-liquibase-defaults-file-respected.sql
@@ -0,0 +1,1 @@
+liquibase update -search-path / -defaults-file &1/sqlcl-liquibase-defaults-file-respected/liquibase.properties -changelog-file &1/sqlcl-liquibase-defaults-file-respected/changelog.xml
