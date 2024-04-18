a8844dd5ab0b370ce9ac2da32126ed43e1e86628
Jason Lyle <jason.lyle@oracle.com>
2024-04-18 14:58:31 -0400

    Add tests for defaults file working relative to script calling liquibase update

---
+++ source/sqlcl_direct_unit_tests/sqlcl-liquibase-default-file-relative-to-script-absolute.sql
@@ -0,0 +1,1 @@
+@ "&2/sqlcl-liquibase-default-file-relative-to-script-absolute/script.sql"
---
+++ source/sqlcl_direct_unit_tests/sqlcl-liquibase-default-file-relative-to-script-relative/script.sql
@@ -0,0 +1,19 @@
+-- Make sure the liquibase changelog table exists
+liquibase validate -database-changelog-table-name &1._changelog -changelog-file changelog.xml
+
+-- Call to see if functionality works
+liquibase update -database-changelog-table-name &1._changelog -changelog-file changelog.xml -defaults-file liquibase.properties
+
+-- See if any changesets were run
+declare
+    c_table_name    constant    varchar2(255 char)  :=  '&1._changelog';
+    l_count                     number;
+begin
+    execute immediate 'select count(1) from ' || c_table_name
+    into l_count;
+
+    if l_count = 0 then
+        raise_application_error(-20001, 'Liquibase update did not run');
+    end if;
+end;
+/
