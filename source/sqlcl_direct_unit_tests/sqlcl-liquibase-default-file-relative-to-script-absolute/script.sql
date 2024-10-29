29a196ff159d324e14eb82f0a8b6bc2abe517008
Jason Lyle <jason.lyle@oracle.com>
2024-10-29 17:00:42 -0400

    Cross os and understanding improvements

--- source/sqlcl_direct_unit_tests/sqlcl-liquibase-default-file-relative-to-script-absolute/script.sql
+++ source/sqlcl_direct_unit_tests/sqlcl-liquibase-default-file-relative-to-script-absolute/script.sql
@@ -1,10 +1,13 @@
 -- Make sure the liquibase changelog table exists
+prompt "Run liquibase validate"
 liquibase validate -database-changelog-table-name &1._changelog -changelog-file changelog.xml
 
 -- Call to see if functionality works
+prompt "Run liquibase update"
 liquibase update -database-changelog-table-name &1._changelog -changelog-file changelog.xml -defaults-file liquibase.properties
 
 -- See if any changesets were run
+prompt "Check changesets ran"
 declare
     c_table_name    constant    varchar2(255 char)  :=  '&1._changelog';
     l_count                     number;
